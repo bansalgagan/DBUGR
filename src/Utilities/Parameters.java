31d112d4cee4c7d52043772dc9aa6d0c30bb51d6
Gagan Bansal <gaganbansal1993@gmail.com>
2014-11-07 18:07:29 +0530

    Added Parameters

---
+++ src/Utilities/Parameters.java
@@ -0,0 +1,23 @@
+package Utilities;
+
+public class Parameters {
+	
+	public final static String RAW_COMMENT_DIR = "data/raw_comments";
+	public final static String MALLET_DIR = "data/mallet";
+	public final static String[] APPS = { "2ch Browser",
+			"ASTRO File Manager with Cloud", "Air Call-Accept free (Necta)",
+			"Antarctic Adventure Free", "Call Dispatcher", "Call-Timer",
+			"Calming Music to Tranquilize", "Cardiograph",
+			"DiDi - Free Calls & Texts", "Easy Browser",
+			"Fast Food Calorie Counter", "File Browser", "Finger Dance Pro",
+			"Floating Browser", "Fun Cinemas", "Fun Math Tricks Lite",
+			"Funny Call", "Keys Jumper Adventure",
+			"Music Therapy for Refreshment",
+			"My Days - Period & Ovulation â?¢", "My Tracks",
+			"Noom CardioTrainer", "Opera Mini â?? Fast web browser",
+			"OvuView: Ovulation & Fertility", "Phone2Phone Internet Calling",
+			"Rebtel: Cheap and Free Calls", "Santa Fun Jokes",
+			"Street Dancer Deluxe Free", "Stretch Exercises",
+			"WikiMobile (Wikipedia Browser)" };
+
+}
