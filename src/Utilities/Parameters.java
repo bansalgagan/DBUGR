b5fa57e63b4f6b3f5c71407b02f6c0c3318d3d5d
Gagan Bansal <gaganbansal1993@gmail.com>
2014-11-20 05:15:31 +0530

    Changed Parameters

--- src/Utilities/Parameters.java
+++ src/Utilities/Parameters.java
@@ -5,19 +5,35 @@ public class Parameters {
 	/**
 	 * data
 	 */
+<<<<<<< HEAD
+	public final static String HEAD_PATH = "/home/btech/cs1110081/Desktop/NLP_PROJECT/DBUGR/";
+	public final static String RAW_COMMENT_DIR = HEAD_PATH + "data/raw-comments";
+	public static final String LABEL_DIR = HEAD_PATH + "data/labels";
+	public static final String BROWN_DIR = HEAD_PATH + "data/brown";
+	public final static String MALLET_DIR = HEAD_PATH + "data/mallet";
+	public final static String POS_DIR = HEAD_PATH + "data/POS";
+=======
 	public final static String RAW_COMMENT_DIR = "/Users/bansal/Desktop/current/NLP_PROJECT/DBUGR/data/raw-comments";
 	public static final String LABEL_DIR = "/Users/bansal/Desktop/current/NLP_PROJECT/DBUGR/data/labels";
 	public static final String BROWN_DIR = "/Users/bansal/Desktop/current/NLP_PROJECT/DBUGR/data/brown";
 	public final static String MALLET_DIR = "/Users/bansal/Desktop/current/NLP_PROJECT/DBUGR/data/mallet";
 	public final static String POS_DIR = "/Users/bansal/Desktop/current/NLP_PROJECT/DBUGR/data/POS";
 	public final static String ACT_DIR = "/Users/bansal/Desktop/current/NLP_PROJECT/DBUGR/data/act";
+>>>>>>> a81d7cad01c18b6a263215f7ab88e0ae81947686
 
 	/**
 	 * libs
 	 */
-	public final static String TWITTER_NLP = "/Users/bansal/Desktop/current/NLP_PROJECT/DBUGR/libs/twitter_nlp";
+	public final static String TWITTER_NLP = HEAD_PATH + "libs/twitter_nlp";
+	public final static String LIBS = HEAD_PATH + "libs";
 
-	public final static String[] APPS = { "ASTRO File Manager with Cloud",
+	public final static String[] APPS = {"OvuView: Ovulation & Fertility", "Parking King",
+		"Parking Manijak", "Race 2 Free", "Raging Thunder 2 - FREE",
+		"Rebtel: Cheap and Free Calls", "SMS Popup", "SoundHound",
+		"Text-To-Speech Extended", "TiKL Touch Talk Walkie Talkie",
+		"Unblock Parking Jam - Free", "WikiMobile (Wikipedia Browser)",
+		"Zello PTT Walkie-Talkie", 
+		"ASTRO File Manager with Cloud",
 			"Air Call-Accept free (Necta)", "AntiVirus Security - FREE",
 			"Audible for Android", "Battle.net Authenticator",
 			"Best Apps Market - for Android", "Burger", "Camera illusion",
@@ -29,12 +45,7 @@ public class Parameters {
 			"Mobile Hidden Camera", "My Days - Period & Ovulation â¢",
 			"My Tracks", "Noom CardioTrainer", "Note Everything",
 			"Opera Mini â Fast web browser",
-			"OvuView: Ovulation & Fertility", "Parking King",
-			"Parking Manijak", "Race 2 Free", "Raging Thunder 2 - FREE",
-			"Rebtel: Cheap and Free Calls", "SMS Popup", "SoundHound",
-			"Text-To-Speech Extended", "TiKL Touch Talk Walkie Talkie",
-			"Unblock Parking Jam - Free", "WikiMobile (Wikipedia Browser)",
-			"Zello PTT Walkie-Talkie" };
+			 };
 	// public final static String[] APPS = { "ASTRO File Manager with Cloud",
 	// "Air Call-Accept free (Necta)", "Antarctic Adventure Free",
 	// "Call Dispatcher", "Call-Timer", "Calming Music to Tranquilize",
